4e91234884a78719f7628fad311c14f82cba9cb4
Issam-Boubcher <boubcher.issam@gmail.com>
2023-04-14 01:12:56 +0000

    Update Logo.tsx

--- components/Icons/Logo.tsx
+++ components/Icons/Logo.tsx
@@ -3,7 +3,7 @@ import logo from "../../public/logo.png";
 export default function Logo() {
   return (
     <Image
-      className="z-10 mx-auto mt-8 md:mt-0"
+      className="z-10 mx-auto mt-8"
       src={logo}
       alt="ENSET Chess Championship logo"
       height={280}
